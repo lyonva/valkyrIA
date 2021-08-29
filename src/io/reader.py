6c631f4728f51e1b0d39a3677b445c75e3fd0611
Leonardo Villalobos Arias <lyon.villalobos@gmail.com>
2021-08-28 21:32:26 -0400

    Add CSV reader implementation

--- src/io/reader.py
+++ src/io/reader.py
@@ -15,5 +15,68 @@ class CSV(Reader):
     
     @staticmethod
     def read_file(path):
-        pass
+        with open(path, 'r',  encoding='utf-8') as file:
+            matrix = []
+            row_length = 0
+            previous = []
+            
+            while line := file.readline():
+                
+                # Remove blanks, remove comments, and split by comma
+                row = line.strip().split("#")[0].split(",")
+                
+                # Strip each element
+                row = [ s.strip() for s in row ]
+                
+                # If we are appending to last row, we recover the last element
+                # Otherwise it makes no difference
+                previous.extend(row)
+                row = previous
+                
+                # If last element is blank, its a split-row
+                if row[-1] == "":
+                    # We have to save
+                    previous = row[:-1]
+                else:
+                    # We have a complete row, save
+                    
+                    # If the length is undefined, its the first row
+                    # We use it as our standard length
+                    if row_length == 0:
+                        row_length = len(row)
+                        
+                    # However, check size
+                    if row_length == len(row):
+                        # Only add if the length matches
+                        matrix.append(row)
+                    previous = []
+                
+            return matrix
+
+
+if __name__ == "__main__":
+    import os
+    import random
+    import string
+    
+    
+    temp_path = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    tf = open(temp_path, "w")
+    
+    # Create 5x5 garbage file
+    # Random text generation from https://stackoverflow.com/questions/2257441/random-string-generation-with-upper-case-letters-and-digits
+    tf.write(
+        "\n".join([
+            ','.join(
+                [''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+                     for i in range(5)]
+                )
+            for i in range(6)])
+        )
+    tf.close()
+    try:
+        res = CSV.read_file(temp_path)
+        print(res)
+    finally:
+        os.remove(temp_path)
     
