68419ab996717ddd9fe54d0aaf5edbd60a9b0356
Leonardo Villalobos Arias <lyon.villalobos@gmail.com>
2021-10-06 12:02:17 -0400

    Adds duration recording to RS FFTF learners.

--- test/test_fft_forest_onrs.py
+++ test/test_fft_forest_onrs.py
@@ -5,16 +5,22 @@ sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
 import pytest
 from src.df import Sample
 from src.ml import FFTForest
+import time
 
 def fftf_dataset(file, depth):
     path = os.path.join("data", file)
     df = Sample.read_csv(path)
 
     print(f"Dataset: {file}")
+    start_time = time.time()
     fftf = FFTForest(df, max_depth = depth)
+    end_time = time.time()
+    duration = end_time - start_time
 
     # Get the best tree
+    print("Done in %.6f ms" % (duration*1000))
     print(fftf.best_tree())
+    
 
 
 def test_fft_forest_rs():
